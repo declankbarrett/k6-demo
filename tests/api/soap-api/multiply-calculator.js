0b77f8228a82baec66eee7615936b4e86211e0ab
Declan Barrett <declan.barrett@kainos.com>
2024-07-16 16:43:30 +0100

    Adding sample script from K6 website

--- tests/api/soap-api/multiply-calculator.js
+++ tests/api/soap-api/multiply-calculator.js
@@ -1,29 +1,29 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-// export const options = {
-//   thresholds: {
-//     http_req_failed: [{ threshold: "rate<0.01", abortOnFail: true }], 
-//     http_req_duration: ['p(99)<1000'], 
-//   },
-//   scenarios: {
-//     // define scenarios
-//     breaking: {
-//       executor: "ramping-vus",
-//       stages: [
-//         { duration: "10s", target: 20 },
-//         { duration: "50s", target: 20 },
-//         { duration: "50s", target: 40 },
-//         { duration: "50s", target: 60 },
-//         { duration: "50s", target: 80 },
-//         { duration: "50s", target: 100 },
-//         { duration: "50s", target: 120 },
-//         { duration: "50s", target: 140 },
-//         //....
-//       ],
-//     },
-//   },
-// };
+export const options = {
+  thresholds: {
+    http_req_failed: [{ threshold: "rate<0.01", abortOnFail: true }], 
+    http_req_duration: ['p(99)<1000'], 
+  },
+  scenarios: {
+    // define scenarios
+    breaking: {
+      executor: "ramping-vus",
+      stages: [
+        { duration: "10s", target: 20 },
+        { duration: "50s", target: 20 },
+        { duration: "50s", target: 40 },
+        { duration: "50s", target: 60 },
+        { duration: "50s", target: 80 },
+        { duration: "50s", target: 100 },
+        { duration: "50s", target: 120 },
+        { duration: "50s", target: 140 },
+        //....
+      ],
+    },
+  },
+};
 
 const numbermultiplicationArray = [
   {"inputOne": 4,"inputTwo":6, "output": 24},
